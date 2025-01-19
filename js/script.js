0d0e9ffb30e8a388d55cb29fbe9adfe370f42ac2
dwisign <dwisign.mail@gmail.com>
2025-01-19 15:52:15 +0700

    update Pertemuan 5 Jquery

--- js/script.js
+++ js/script.js
@@ -92,4 +92,103 @@ const cars = [
     'Tesla'
 ]
 
+const motorBike = [
+    'nmax',
+    'adv',
+    'pcx',
+    'aerox',
+    'vario',
+    'ducati'
+]
+
 console.log("Car Brand: " , cars)
+console.table(cars)
+console.log('Data Index ke-4: ', cars[4])
+console.log('Jumlah Data: ', cars.length)
+
+
+//ARRAY METHODS
+cars.push('BMW') //PUSH
+console.table(cars)
+
+const newData = cars.toString() // toString
+console.log(newData)
+
+const newDataJoin = cars.join(" - ") //join
+console.log(newDataJoin)
+
+const newDataShift = cars.shift() // shift
+console.log(newDataShift)
+console.log(cars)
+
+delete cars[3] // delete
+console.log(cars)
+console.table(cars)
+
+const concatData = cars.concat(motorBike) //concat
+console.table(concatData)
+
+
+//LOOPING
+for(i = 0; i < cars.length; i++ ){
+    console.log('HASIL LOOPING: ', cars[i])
+}
+
+//FOOD
+const food = [
+    {
+        id : 1,
+        rasa: 'Manis',
+        warna: 'Merah',
+        bentuk: 'Bulat'
+    },
+    {
+        id : 2,
+        rasa: 'Pahit',
+        warna: 'Kuning',
+        bentuk: 'Kotak'
+    },
+    {
+        id : 3,
+        rasa: 'Asam',
+        warna: 'Hijau',
+        bentuk: 'Bulat'
+    }
+]
+
+food.forEach(foods => {
+    for(let value in foods){
+        console.log(`${foods[value]}`)
+    }
+})
+
+
+//JQUERY SCRIPT
+$(document).ready(function(){
+
+    //SHOW HIDE
+    $('#textReadMore').hide()
+
+    $('#showText').click(function(){
+        $('#textReadMore').show()
+    })
+
+    $('#hideText').click(function(){
+        $('#textReadMore').hide()
+    })
+
+
+    //TOGGLE
+    $('#textServices').hide()
+    $('#showServices').click(function(){
+        $('#textServices').toggle()
+        if($(this).text() == 'Hide Services'){
+            $(this).text('Show More Services →')
+        }else{
+            $(this).text('Hide Services')
+        }
+    })
+
+
+})
+
