98b652214109c67cc046beb09526fddaa88f5232
dwisign <dwisign.mail@gmail.com>
2025-01-26 16:17:45 +0700

    Update Last Session

--- js/script.js
+++ js/script.js
@@ -188,7 +188,25 @@ $(document).ready(function(){
             $(this).text('Hide Services')
         }
     })
-
-
 })
+
+
+//SPLIDE JS
+var splide = new Splide( '.splide', {
+    perPage: 3,
+    gap    : '2rem',
+    breakpoints: {
+      640: {
+        perPage: 2,
+        gap    : '.7rem',
+        height : '6rem',
+      },
+      480: {
+        perPage: 1,
+        gap    : '.7rem',
+        height : '6rem',
+      },
+    },
+});
+splide.mount();
 
